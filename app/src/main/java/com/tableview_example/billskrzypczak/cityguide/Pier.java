0408e4cb51a1aca39356e8fdfaa2f39f2cf869c2
Bill Skrzypczak <bskrzypczak@mchenry.edu>
2016-09-15 16:45:54 -0500

    Initial commit

---
+++ app/src/main/java/com/tableview_example/billskrzypczak/cityguide/Pier.java
@@ -0,0 +1,14 @@
+package com.tableview_example.billskrzypczak.cityguide;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Pier extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pier);
+    }
+
+}
